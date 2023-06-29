518c1de71e20a29c21d4c3454bd5f40dc44d7c64
Meda Vishnu Datta <93989286+vishnudatta0341@users.noreply.github.com>
2023-06-29 19:59:18 +0530

    Update login.js

--- login.js
+++ login.js
@@ -3,6 +3,7 @@ var y = localStorage.setItem("pass", "qaifi");
 function authen() {
   var email = document.getElementById("username").value;
   var password = document.getElementById("password").value;
+  password.type="text";
   if (
     email == localStorage.getItem('user') &&
     password == localStorage.getItem('pass')
